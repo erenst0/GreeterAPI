5d404131f36f0656eb1d9d78685c7121b2bc5de8
Ernesto <ernesto.chirinos@gmail.com>
2017-04-17 23:22:40 -0400

    Initial commit

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Ernesto on 2016-07-06.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
